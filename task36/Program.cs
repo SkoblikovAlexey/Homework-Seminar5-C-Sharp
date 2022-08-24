ffb7edc60e2a55a325f32c0b8a787bace237d923
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-24 08:46:55 +0300

    Task 36 complete

--- task36/Program.cs
+++ task36/Program.cs
@@ -8,41 +8,44 @@
 while (true)
 {
     int[] CreateArray(int N)
-{
-    int[] array = new int[N];
-    for (int i = 0; i < N; i++)
     {
-        array[i] = new Random().Next(-100, 100);
-    }
-    return array;
-}
-void PrintArray(int[] arr)
-{
-    int length = arr.Length;
-    Console.Write("[");
-    for (int i = 0; i < length - 1; i++)
-    {
-        Console.Write(arr[i] + ", ");
-    }
-    Console.WriteLine(arr[length - 1] + "]");
-}
-
-int SummOddNumbers(int[] arr){
-    int summ = 0;
-    for (int i = 0; i < arr.Length; i++) {
-        if(i % 2 == 1) {
-            summ += arr[i];
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            array[i] = new Random().Next(-100, 100);
         }
+        return array;
     }
-    return summ;
-}
+    void PrintArray(int[] arr)
+    {
+        int length = arr.Length;
+        Console.Write("[");
+        for (int i = 0; i < length - 1; i++)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+        Console.WriteLine(arr[length - 1] + "]");
+    }
 
-Console.WriteLine("Введите количество элементов массива.");
-int N = Convert.ToInt32(Console.ReadLine());
+    int SummOddNumbers(int[] arr)
+    {
+        int summ = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i % 2 == 1)
+            {
+                summ += arr[i];
+            }
+        }
+        return summ;
+    }
 
-int[] array = CreateArray(N);
-PrintArray(array);
-Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях равна {SummOddNumbers(array)}");
+    Console.WriteLine("Введите количество элементов массива.");
+    int N = Convert.ToInt32(Console.ReadLine());
+
+    int[] array = CreateArray(N);
+    PrintArray(array);
+    Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях равна {SummOddNumbers(array)}");
 
     Console.WriteLine("Для завершения введите end, для продолжения - любой символ");
     string a = Console.ReadLine();
